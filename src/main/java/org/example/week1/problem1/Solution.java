285a9068bf76a724088f0556c94c254f0d273059
kyupid <iamscvv@gmail.com>
2024-01-04 00:39:38 +0900

    가장 편한 방법으로 구현

--- src/main/java/org/example/week1/problem1/Solution.java
+++ src/main/java/org/example/week1/problem1/Solution.java
@@ -1,7 +1,10 @@
 package org.example.week1.problem1;
 
+import java.util.Arrays;
+
 public class Solution {
-    public static int sum(int[] ints) {
-        return 0;
+    public static int sum(int[] arr) {
+        return Arrays.stream(arr)
+                .sum();
     }
 }
