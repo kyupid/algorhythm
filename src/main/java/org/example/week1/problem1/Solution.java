312526478bcea318471fd333b13c807064c48635
kyupid <iamscvv@gmail.com>
2024-01-04 00:58:19 +0900

    재귀함수로 구현

--- src/main/java/org/example/week1/problem1/Solution.java
+++ src/main/java/org/example/week1/problem1/Solution.java
@@ -4,7 +4,10 @@ import java.util.Arrays;
 
 public class Solution {
     public static int sum(int[] arr) {
-        return Arrays.stream(arr)
-                .sum();
+        if (arr.length == 0) {
+            return 0;
+        }
+        int[] copy = Arrays.copyOfRange(arr, 1, arr.length);
+        return arr[0] + sum(copy);
     }
 }
