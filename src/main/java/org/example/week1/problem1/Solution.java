0dd16eb1a266a6f72ce9eb6a380e75bdbb7a781b
kyupid <iamscvv@gmail.com>
2024-01-05 00:13:35 +0900

    꼬리 재귀함수로 구현

--- src/main/java/org/example/week1/problem1/Solution.java
+++ src/main/java/org/example/week1/problem1/Solution.java
@@ -4,10 +4,14 @@ import java.util.Arrays;
 
 public class Solution {
     public static int sum(int[] arr) {
+        return sum(arr, 0);
+    }
+
+    private static int sum(int[] arr, int acc) {
         if (arr.length == 0) {
-            return 0;
+            return acc;
         }
         int[] copy = Arrays.copyOfRange(arr, 1, arr.length);
-        return arr[0] + sum(copy);
+        return sum(copy, acc + arr[0]);
     }
 }
