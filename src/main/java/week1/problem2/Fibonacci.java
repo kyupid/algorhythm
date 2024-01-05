a73ee7930e6037c4387525e814fc59b9d3ef1c75
kyupid <iamscvv@gmail.com>
2024-01-06 01:10:00 +0900

    [피보나치] 편한 방법

--- src/main/java/week1/problem2/Fibonacci.java
+++ src/main/java/week1/problem2/Fibonacci.java
@@ -2,6 +2,19 @@ package week1.problem2;
 
 public class Fibonacci {
     public static int calculate(int i) {
-        return 0;
+        // 0, 1, 1, 2, 3, 5, 8, 13, ...
+        if (i <= 0) {
+            return 0;
+        }
+        int first = 0;
+        int second = 1;
+        int result;
+
+        for (int j = 0; j < i; j++) {
+            result = first + second;
+            first = second;
+            second = result;
+        }
+        return first;
     }
 }
