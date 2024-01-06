31ee02048a540d9574d994706e7ad432a09b0716
kyupid <iamscvv@gmail.com>
2024-01-06 15:14:23 +0900

    [week1-problem2] 피보나치 꼬리 재귀

--- src/main/java/week1/problem2/Fibonacci.java
+++ src/main/java/week1/problem2/Fibonacci.java
@@ -1,15 +1,23 @@
 package week1.problem2;
 
 public class Fibonacci {
-    public static int calculate(int i) {
-        // 0, 1, 1, 2, 3, 5, 8, 13, ...
-        if (i <= 0) {
+
+    public static int calculate(int n) {
+        if (n <= 0) {
             return 0;
         }
-        if (i == 1) {
-            return 1;
+        return calculateTailRec(0, 1, n);
+        // 1 -> 1
+    }
+
+    private static int calculateTailRec(int a, int b, int count) {
+        // 종료 조건 도달하기 위한 count
+        count--;
+
+        // 종료 조건
+        if (count == 0) {
+            return b;
         }
-        // todo if i > 1
-        return calculate(i - 1) + calculate(i - 2);
+        return calculateTailRec(b, a + b, count);
     }
 }
